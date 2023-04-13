310b87dc967514e241cff123d524cf96919b6870
samularity <samuelmunz@gmx.de>
2023-04-13 23:45:15 +0200

    fix version of libssh2 for now
    last working version is 463449fb9ee7dbe5fbe71a28494579a9a6890d6d

--- patches/sdk_patches.py
+++ patches/sdk_patches.py
@@ -13,12 +13,16 @@ target_dir = path.join(frwdir, "components", "libssh2" )
 if not path.exists(target_dir):
     print (f"patching {framework} located in {frwdir} with {repo_url}")
     env.Execute(f"git clone {repo_url} {target_dir}")
-    #update CMakeLists and libssh2_config with own version
-    env.Execute(f"cp $PROJECT_DIR/patches/CMakeLists.txt {target_dir}/CMakeLists.txt")
-    env.Execute(f"cp $PROJECT_DIR/patches/libssh2_config.h {target_dir}/include/libssh2_config.h")
+    env.Execute(f'git -C "{target_dir}"  pull --rebase {repo_url} 463449fb9ee7dbe5fbe71a28494579a9a6890d6d')
 
 else:
     print (f"checking updates for {target_dir} in {repo_url}")
-    print ("\t--> not implemented yet... ")
+    env.Execute(f'git -C "{target_dir}" reset --hard origin/master')
+    #env.Execute(f'git -C "{target_dir}"  pull --rebase {repo_url}')
+    env.Execute(f'git -C "{target_dir}"  pull --rebase {repo_url} 463449fb9ee7dbe5fbe71a28494579a9a6890d6d')
+
+#update CMakeLists and libssh2_config with own version
+env.Execute(f"cp $PROJECT_DIR/patches/CMakeLists.txt {target_dir}/CMakeLists.txt")
+env.Execute(f"cp $PROJECT_DIR/patches/libssh2_config.h {target_dir}/include/libssh2_config.h")
 
 print("====== CUSTOM SCRIPT DONE =====")
